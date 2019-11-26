24c70db95d82215405870de477c8e60f3879cbd8
dinobaranasic <dino.baranasic8@gmail.com>
2019-11-26 17:10:04 +0100

    Početna faza.

--- Dizalo/Form1.Designer.cs
+++ Dizalo/Form1.Designer.cs
@@ -28,13 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label_trenutnikat = new System.Windows.Forms.Label();
+            this.label_trenutnoosoba = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label_trenutnikat
+            // 
+            this.label_trenutnikat.AutoSize = true;
+            this.label_trenutnikat.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label_trenutnikat.Location = new System.Drawing.Point(689, 236);
+            this.label_trenutnikat.Name = "label_trenutnikat";
+            this.label_trenutnikat.Size = new System.Drawing.Size(80, 13);
+            this.label_trenutnikat.TabIndex = 0;
+            this.label_trenutnikat.Text = "Trenutni kat:";
+            // 
+            // label_trenutnoosoba
+            // 
+            this.label_trenutnoosoba.AutoSize = true;
+            this.label_trenutnoosoba.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label_trenutnoosoba.Location = new System.Drawing.Point(689, 328);
+            this.label_trenutnoosoba.Name = "label_trenutnoosoba";
+            this.label_trenutnoosoba.Size = new System.Drawing.Size(148, 13);
+            this.label_trenutnoosoba.TabIndex = 1;
+            this.label_trenutnoosoba.Text = "Trenutno osoba u dizalu:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(692, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(236, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "U dizalo može biti maksimalno 10 osoba.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(695, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(240, 26);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Prema redosljedu pozivanja dizala ,dizalo\r\n obavlja svoju funkciju.";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(820, 721);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Dino Baranašić";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(963, 750);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label_trenutnoosoba);
+            this.Controls.Add(this.label_trenutnikat);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Dizalo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label_trenutnikat;
+        private System.Windows.Forms.Label label_trenutnoosoba;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
     }
 }
 
